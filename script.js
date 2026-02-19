96ed8f3c3f6b73d7ea4b3376848cc8deeb38890b
Rahul <rahulofficial7652@gmail.com>
2026-02-19 14:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const dateElements = document.getElementsByClassName("date");
+const just = new Date();
+
+const format = just.toLocaleString('en-US', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: true
+});
+
+const answer = format.replace(',', "");
+
+dateElements[0].innerText = answer;
